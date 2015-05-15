8a0967c08fec606482122b05ea11245d630dd32d
Steven <example@example.com>
2015-05-15 01:02:04 +0100

    completed moving tests to buster

--- test/riffle-test.js
+++ test/riffle-test.js
@@ -3,58 +3,57 @@ var ok = buster.referee.assert;
 var _ = require('underscore');
 var stream = require('../lib/riffle');
 
-
-var after = function (times, cb) {
-  var i = 0;
-  return function () {
-    i++;
-    if (i >= times) cb();
-  };
-};
-
 buster.testCase('Stream API', {
-  setUp : function () {
-    this.timeout = 10000;
-  },
-
   'Stream, invoke, onOutput': function (cb) {
-    var start = after(1, cb);
+    var spy = this.spy();
   
     var s = stream()
       .onOutput(function (a) {
         ok(a.args[0] === 2, 'onOutput correctly receives value passed to invoked stream');
-        start();
+        spy();
       });
-    setTimeout(function () { s.invoke(2); }, 100);
+    setTimeout(function () {
+      s.invoke(2);
+    }, 100);
+    setTimeout(function () {
+      ok(spy.callCount, 1, 'Output only called once');
+      cb();
+    }, 200);
   },
 
   'Repeat invoke': function (cb) {
-    var start = after(1, cb);
+    var spy = this.spy();
     var seen = new Array(6);
     var s = stream()
       .onOutput(function (x) {
         seen[x.args[0]] = true;
         ok(x.args[0] === 3 || x.args[0] === 6, 'onOutput correctly receives value passed to invoked stream');
-        if (seen[6] && seen[3]) start();
+        if (seen[6] && seen[3]) spy();
       });
     s.invoke(6);
     s.invoke(3);
+    setTimeout(function () {
+      ok(spy.callCount, 1, 'Output only called once');
+      cb();
+    }, 200);
   },
 
   'Invoke should send each argument to stream in its own event loop': function (cb) {
-    var start = after(1, cb); // TODO how to error if called more than expecting?
-    
+    var spy = this.spy();
     var s = stream()
       .onOutput(function () {
         ok(true);
-        start();
+        spy();
       });
     s.invoke(6, 5, 4, 3, 2, 1);
-    setTimeout(function () { start(); }, 500);
+    setTimeout(function () {
+      ok(spy.callCount, 1, 'Output only called once');
+      cb();
+    }, 200);
   },
 
   'Multiple input streams funnel together': function (cb) {
-    var start = after(1, cb);
+    var spy = this.spy();
     var a = stream();
     var b = stream();
     var seen = new Array(6);
@@ -64,17 +63,21 @@ buster.testCase('Stream API', {
         seen[x] = true;
         ok(x === 3 || x === 4, 'onOutput correctly receives value through chain of streams');
         if (seen[3] && seen[4]) {
-          start();
+          spy();
         }
       });
     s.input(a);
     s.input(b);
     a.invoke(3);
     b.invoke(4);
+    setTimeout(function () {
+      ok(spy.callCount, 1, 'Multiple calls to input correctly funnels streams together');
+      cb();
+    }, 200);
   },
 
   'Multiple input streams funnel together (array argument)': function (cb) {
-    var start = after(1, cb);
+    var spy = this.spy();
     var a = stream();
     var b = stream();
     var seen = new Array(6);
@@ -84,51 +87,58 @@ buster.testCase('Stream API', {
         seen[x] = true;
         ok(x === 3 || x === 4, 'onOutput correctly receives value through chain of streams');
         if (seen[3] && seen[4]) {
-          start();
+          spy();
         }
       });
     s.input([a, b]);
     a.invoke(3);
     b.invoke(4);
+    setTimeout(function () {
+      ok(spy.callCount, 1, 'Multi-input notation correctly funnels streams together');
+      cb();
+    }, 200);
   },
 
   'Multiple identical input streams de-duped': function (cb) {
-    var start = after(1, cb);
+    var spy = this.spy();
     var a = stream();
     var seen = 0;
     var s = stream()
       .onOutput(function (_x) {
         var x = _x.args[0];
-        if (x === 2) {
-          seen++;
-        }
+        spy();
         ok(true, 'onOutput correctly receives value through chain of streams');
-        if (x !== 2 && seen === 1) {
-          start();
-        }
       });
     s.input([a, a]);
     a.invoke(2);
-    setTimeout(function () { a.invoke(3); }, 3000);
+    setTimeout(function () { a.invoke(3); }, 200);
+    setTimeout(function () {
+      ok(spy.callCount, 2, 'onOutput correctly unions all streams');
+      cb();
+    }, 200);
   },
 
   'Input with non-stream arguments should be ignored': function (cb) {
-    var start = after(1, cb);
+    var spy = this.spy();
     var a = stream();
     var seen = 0;
     var s = stream()
       .onOutput(function (_x, _y) {
         var x = _x.args[0];      
         ok(x === 2 && typeof _y === 'undefined', 'onOutput correctly receives value through chain of streams');
+        spy();
       });
     s.input(a);
     s.input(3, 1);
     a.invoke(2);
-    setTimeout(function () { start(); }, 500);
+    setTimeout(function () {
+      ok(spy.callCount, 1, 'onOutput correctly unions all streams');
+      cb();
+    }, 200);
   },
 
   'Replace existing inputs by calling input': function (cb) {
-    var start = after(1, cb);
+    var spy = this.spy();
     var a = stream();
     var b = stream();
     var c = stream();
@@ -137,9 +147,9 @@ buster.testCase('Stream API', {
       .onOutput(function (_x, _y) {
         var x = _x.args[0];
         ok(typeof _y === 'undefined', 'onOutput correctly receives value through chain of streams');
-        if (x === 1) return seen++;
-        if (x === 2) return seen++;
-        if (x === 3) return seen++;
+        if (x === 1) return spy();
+        if (x === 2) return spy();
+        if (x === 3) return spy();
         return null;
       });
     s.input([a, b]);
@@ -148,40 +158,47 @@ buster.testCase('Stream API', {
     setTimeout(function () { a.invoke(1); }, 50);
     setTimeout(function () { b.invoke(2); }, 50);
     setTimeout(function () { c.invoke(3); }, 50);
-    setTimeout(function () { ok(seen === 3, 'onOutput correctly unions all streams'); start(); }, 500);
+    setTimeout(function () {
+      ok(spy.callCount, 3, 'onOutput correctly unions all streams');
+      cb();
+    }, 200);
   },
 
   'Multiple onOutput': function (cb) {
-    var start = after(3, cb);
+    var spy = this.spy();
     var p = stream();
     var s = stream()
-        .onOutput(function(a) {
-          ok(a.args[0] === 2, 'First onOutput receives value passed to invoked stream');
-          start();
-        })
-        .onOutput(function(a) {
-          ok(a.args[0] === 2, 'Second onOutput receives value passed to invoked stream');
-          start();
-        });
+      .onOutput(function(a) {
+        ok(a.args[0] === 2, 'First onOutput receives value passed to invoked stream');
+        spy();
+      })
+      .onOutput(function(a) {
+        ok(a.args[0] === 2, 'Second onOutput receives value passed to invoked stream');
+        spy();
+      });
     p.input(s)
-        .onOutput(function(a) {
-          ok(a.args[0] === 2, 'onOutput receives value passed to dependent stream');
-          start();
-        });
+      .onOutput(function(a) {
+        ok(a.args[0] === 2, 'onOutput receives value passed to dependent stream');
+        spy();
+      });
     setTimeout(function () { s.invoke(2); }, 100);
+    setTimeout(function () {
+      ok.equals(spy.callCount, 3, 'Stream only called once');
+      cb();
+    }, 200);
   },
 
   'On onOutput exception, crash interal to stream, execute other onOutputs (should never happen)': function(cb) {
-    var start = after(3, cb);
+    var spy = this.spy();
     try {
       var s = stream()
         .onOutput(function (a) {
-          start();
+          spy();
           throw "test";
         })
         .onOutput(function (a) {
           ok(true, 'Second onOutput receives value passed to invoked stream when first onOutput throws an exception');
-          start();
+          spy();
         });
       setTimeout(function () {
         try {
@@ -194,12 +211,13 @@ buster.testCase('Stream API', {
       ok(false, 'Stream construct scope does not catch an exception thrown in onOutput');
     }
     setTimeout(function () {
-      start();
+      ok.equals(spy.callCount, 2, 'Stream only called once');
+      cb();
     }, 100);
   },
 
   'Multiple input streams to multiple ports': function (cb) {
-    var start = after(1, cb);
+    var spy = this.spy();
     var a = stream();
     var b = stream();
     var _a;
@@ -208,41 +226,46 @@ buster.testCase('Stream API', {
       _a = _a || a;
       _b = _b || b;
       if (_a && _b) {
+        spy();
         ok((_a + _b) === 7, 'Stream received values from multiple input ports');
-        start();
+        cb();
       }
     });
     s.input(a, 0);
     s.input(b, 1);
     a.invoke(3);
     b.invoke(4);
+    setTimeout(function () {
+      ok.equals(spy.callCount, 1, 'Stream only called once');
+    }, 50);
   },
   
-  '// Multiple input streams to single port, 2': function () {
-    var start = sinon.spy();
+  'Multiple input streams to single port': function (cb) {
+    var spy = this.spy();
     var a = stream();
     var b = stream();
-    var seen = new Array(6);
+    var seen = {};
     var s = stream(function (output, x) {
       seen[x] = true;
-      ok(true, 'Stream received value a single time');
-      if (seen[3] && seen[4]) {
-        start();
+      spy();
+      if (seen['a'] && seen['b']) {
+        ok(spy.callCount === 2, 'Stream received an input from both streams on a single port');
+        cb();
       }
     });
     s.input([a, b]);
-    a.invoke(3);
-    b.invoke(4);
+    a.invoke('a');
+    b.invoke('b');
   },
 
-  '// Exception, crash interally. onOutput still runs, 1': function() {
+  'Exception, crash interally. onOutput still runs': function(cb) {
     try {
       var s = stream(function (output, a) {
         output(a);
         throw "test";
       })
       .onOutput(function (a) {
-        ok(a === 'a', 'onOutput receives value correctly when stream throws an exception');
+        ok(a.args[0] === 'a', 'onOutput receives value correctly when stream throws an exception');
       });
       setTimeout(function () {
         try {
@@ -255,31 +278,31 @@ buster.testCase('Stream API', {
       ok(false, 'Stream constructor scope does not catch an exception thrown in stream function');
     }
     setTimeout(function () {
-      start();
+      cb();
     }, 100);
   },
 
-  '// Stream output becomes onOutput param, 1': function () {
+  'Stream output becomes onOutput param': function (cb) {
     var a = stream();
     var b = stream();
-    var _a;
-    var _b;
-    var plus = stream(function (output, a, b) {
-      _a = _a || a;
-      _b = _b || b;
-      output(_a + _b, 'foo');
+    var aSave;
+    var bSave;
+    var plus = stream(function (output, _a, _b) {
+      aSave = aSave || _a;
+      bSave = bSave || _b;
+      if (aSave && bSave) output(aSave + bSave, 'foo');
     });
-    plus.input(a, b);
+    plus.input(a, 0);
+    plus.input(b, 1);
     plus.onOutput(function (result, foo) {
-      if (isNaN(result)) { return; }
-      ok(result === 5 && foo === 'foo', 'Stream function correctly passes multiple values to onOutput via output continuation function');
-      start();
+      ok(result.args[0] === 5 && typeof foo === 'undefined', 'Stream function correctly passes a single value to onOutput via output continuation function');
+      cb();
     });
-    setTimeout(function () { a.invoke(2) }, 100);
-    setTimeout(function () { b.invoke(3) }, 100);
+    setTimeout(function () { a.invoke(2); }, 100);
+    setTimeout(function () { b.invoke(3); }, 100);
   },
 
-  '// offOutput unregisters onOutput callback, 1': function () {
+  'offOutput unregisters onOutput callback': function (cb) {
     var handler = function () {
       ok(false, 'Handler should not be called after being unbound by offOutput');
     };
@@ -287,14 +310,14 @@ buster.testCase('Stream API', {
     a.onOutput(handler);
     a.offOutput(handler);
 
-    setTimeout(function () { a.invoke() }, 50);
+    setTimeout(function () { a.invoke(); }, 50);
     setTimeout(function () {
       ok(true, 'After a period of time elapses, handler should not be called because it was unbound by offOutput');
-      start();
+      cb();
     }, 100);
   },
 
-  '// Composition, 1': function () {
+  'Composition': function (cb) {
     var a = stream();
     var b = stream();
     var c = stream();
@@ -316,86 +339,90 @@ buster.testCase('Stream API', {
         output(_a * _b);
       });
     }());
-    times.input(c, plus.input(a, b));
+    plus.input(a);
+    plus.input(b, 1);
+    times.input(c);
+    times.input(plus, 1);
     times.onOutput(function (result) {
-      if (isNaN(result)) { return; }
-      ok(result === 20, 'onOutput correctly receives value of composed math operations');
-      start();
+      if (isNaN(result.args[0])) { return; }
+      ok.same(result.args[0], 20, 'onOutput correctly receives value of composed math operations');
+      cb();
     });
-    setTimeout(function () { a.invoke(2) }, 100);
-    setTimeout(function () { b.invoke(3) }, 100);
-    setTimeout(function () { c.invoke(4) }, 100);
+    setTimeout(function () { a.invoke(2); }, 100);
+    setTimeout(function () { b.invoke(3); }, 100);
+    setTimeout(function () { c.invoke(4); }, 100);
   }
 });
 
 buster.testCase('Pathologically streaming', {
-
-  '// Join onOutput, 2': function () {
-    var a = stream.promise();
-    var b = stream.promise();
-    var abJoin = stream.join(a, b);
-    setTimeout(function () { a.fulfill(2); }, 50);
-    setTimeout(function () { b.fulfill(3); }, 100);
-    abJoin.onOutput(function(aVal, bVal) {
-        ok(aVal === 2, 'Join passes the first parameter');
-        ok(bVal === 3, 'Join passes the second parameter');
-        start();
+  'Join onOutput': function (cb) {
+    var spy = this.spy();
+    var a = stream();
+    var b = stream();
+    setTimeout(function () { a.invoke(2); }, 50);
+    setTimeout(function () { b.invoke(3); }, 100);
+    var abJoin = stream.join(function(out, aVal, bVal) {
+      spy();
+      ok(aVal.args[0] === 2, 'Join passes the first parameter');
+      ok(bVal.args[1] === 3, 'Join passes the second parameter');
     });
-    stop(3000);
+    abJoin.input(a);
+    abJoin.input(b, 1);
+    setTimeout(function () {
+      ok.equals(spy.callCount, 1, 'Join only called once');
+      cb();
+    }, 200);
   },
 
-  '// Join zero arguments, 1': function () {
+  'Join zero arguments': function (cb) {
     var z = stream.join();
     z.onOutput(function (aVal) {
         ok(false, 'Joining nothing should not invoke onOutput handlers');
     });
     setTimeout(function () {
         ok(true, 'Joining nothing should not invoke output handlers');
-        start();
-    }, 500);
-    stop(3000);
+        cb();
+    }, 200);
   },
 
-  '// Function composition, 1': function () {
+  'Function composition': function (cb) {
     var a = stream();
     var b = stream();
     var c = stream();
     var plus = stream.func(function (a, b) {
-        return a + b;
+      return a + b;
     });
     var times = stream.func(function (a, b) {
-        return a * b;
+      return a * b;
     });
     var result = times(c, plus(a, b));
     result.onOutput(function (result) {
-            if (isNaN(result)) { return; }
-            ok(result === 20, 'onOutput correctly receives value of composed math operations');
-            start();
-        });
-    setTimeout(function () { a.invoke(2) }, 100);
-    setTimeout(function () { b.invoke(3) }, 100);
-    setTimeout(function () { c.invoke(4) }, 100);
-    stop(3000);
+      if (isNaN(result.args[0])) { return; }
+      ok(result.args[0] === 20, 'onOutput correctly receives value of composed math operations');
+      cb();
+    });
+    setTimeout(function () { a.invoke(2); }, 100);
+    setTimeout(function () { b.invoke(3); }, 100);
+    setTimeout(function () { c.invoke(4); }, 100);
   },
 
-  '// Mixed parameter types, 1': function () {
-    var a = stream.promise();
+  'Mixed parameter types': function (cb) {
+    var a = stream();
     var b = 3;
     var c = stream();
     var plus = stream.func(function (a, b) {
-        return a + b;
+      return a + b;
     });
     var times = stream.func(function (a, b) {
-        return a * b;
+      return a * b;
     });
     var result = times(c, plus(a, b));
     result.onOutput(function (result) {
-        if (isNaN(result)) { return; }
-        ok(result === 20, 'onOutput correctly receives value of composed math operations');
-        start();
+      if (isNaN(result.args[0])) { return; }
+      ok(result.args[0] === 20, 'onOutput correctly receives value of composed math operations');
+      cb();
     });
-    setTimeout(function () { a.fulfill(2) }, 100);
-    setTimeout(function () { c.invoke(4) }, 100);
-    stop(3000);
+    setTimeout(function () { a.invoke(2); }, 100);
+    setTimeout(function () { c.invoke(4); }, 100);
   }
 });
